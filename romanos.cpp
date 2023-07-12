570aec1e6e37f56859ec74f4a5f2c72c685c421b
David Herbert <davidherbertsb@gmail.com>
2023-07-12 14:48:53 -0300

    Testes 3 invalidos e validos

--- romanos.cpp
+++ romanos.cpp
@@ -20,6 +20,24 @@ string decimal_para_romanos(int num) {
 }
 
 
+int romanos_para_decimal(string str) {
+  int tam = str.size();
+  map <char, int> M; 
+  M.insert(pair<char, int> ('I', 1));
+  M.insert(pair<char, int> ('V', 5));
+  M.insert(pair<char, int> ('X', 10));
+  M.insert(pair<char, int> ('L', 50));
+  M.insert(pair<char, int> ('C', 100));
+  M.insert(pair<char, int> ('D', 500));
+  M.insert(pair<char, int> ('M', 1000));
+  if(str.size() > 30) return -1;
+  int res = 0;
+  for (int i = 0; i < tam; i++) {
+    if(M[str[i]] < M[str[i+1]]) res -= M[str[i]];
+    else res += M[str[i]];
+  }
+    return res;
+}
 
 int validate_convert(string str) {
   int guess = romanos_para_decimal(str);
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -12,3 +12,19 @@ TEST_CASE( "Numeros romanos - algarismos unicos", "[romanos]" ) {
     REQUIRE(validate_convert("D") == 500);
     REQUIRE(validate_convert("M") == 1000);
 }
+TEST_CASE( "Numeros romanos - algarismos invalidos", "[romanos]" ) {
+    REQUIRE(validate_convert("G") == -1 );
+    REQUIRE(validate_convert("IXI") == -1);
+    REQUIRE(validate_convert("XVV") == -1);
+    REQUIRE(validate_convert("MMIM") == -1);
+    REQUIRE(validate_convert("XVIV") == -1);
+}
+TEST_CASE( "Numeros romanos - algarismos validos", "[romanos]" ){
+    REQUIRE(validate_convert("XV") == 15);
+    REQUIRE(validate_convert("XIX") == 19);
+    REQUIRE(validate_convert("MMM") == 3000);
+    REQUIRE(validate_convert("CDXXVII") == 427);
+    REQUIRE(validate_convert("DCXXXVII") == 637);
+    REQUIRE(validate_convert("MMDI") == 2501);
+
+}
