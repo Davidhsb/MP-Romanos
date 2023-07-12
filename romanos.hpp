b062c99a4f56f400da3e71c0925cd371f5f8c552
David Herbert <davidherbertsb@gmail.com>
2023-07-12 14:50:45 -0300

    Testes 4 códigos completo

--- romanos.hpp
+++ romanos.hpp
@@ -1,5 +1,11 @@
 #ifndef ROMANOS_HPP
 #define ROMANOS_HPP
 
+#include <map>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int validate_convert(string str);
 
 #endif // ROMANOS_HPP
