5be2facc0062a98c550a594988cdf689270bd024
David Herbert <davidherbertsb@gmail.com>
2023-07-12 14:53:36 -0300

    Testes 6-adicionando mais testes

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -19,7 +19,7 @@ TEST_CASE( "Numeros romanos - algarismos invalidos", "[romanos]" ) {
     REQUIRE(validate_convert("MMIM") == -1);
     REQUIRE(validate_convert("XVIV") == -1);
     REQUIRE(validate_convert("XXXXXX") == -1);
-    
+    REQUIRE(validate_convert("XVIX") == -1);
 }
 TEST_CASE( "Numeros romanos - algarismos validos", "[romanos]" ){
     REQUIRE(validate_convert("XV") == 15);
